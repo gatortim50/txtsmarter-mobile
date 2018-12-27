27f775b026eb84ad643e25b703ff789cf3384141
Tim <tim@example.com>
2018-12-27 18:35:04 -0500

    add login navigator

---
+++ src/navigators/LoginNavigator.js
@@ -0,0 +1,26 @@
+import { createStackNavigator,createAppContainer } from "react-navigation";
+
+import Login from '../components/auth/Login';
+import SignUp from '../components/signUp/SignUp';
+
+const LoginNavigator = createStackNavigator(
+    {
+        login: {
+            screen: Login,
+            navigationOptions:{
+                header:null,
+            }
+        },
+      signUp: {
+        screen: SignUp,
+        navigationOptions:{
+          header:null,
+        }
+      },
+    },
+    {
+        initialRouteName: "login",
+    }
+);
+
+export default createAppContainer(LoginNavigator);
--- src/navigators/SignUpNavigator.js
+++ src/navigators/SignUpNavigator.js
@@ -1,19 +1,11 @@
 import { createStackNavigator,createAppContainer } from "react-navigation";
 
-import Login from '../components/auth/Login';
-
 import SignUp from '../components/signUp/SignUp';
 import SignUp2 from '../components/signUp/SignUp2';
 import SignUp3 from '../components/signUp/SignUp3';
 
 const SignUpNavigator = createStackNavigator(
     {
-        login: {
-            screen: Login,
-            navigationOptions:{
-                header:null,
-            }
-        },
       signUp: {
         screen: SignUp,
         navigationOptions:{
